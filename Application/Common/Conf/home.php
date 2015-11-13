d4df32344a28f4268b9666db3b02b41222f8aa93
lanzhanqiao <lanzhanqiao@163.com>
2015-11-13 13:50:16 +0800

    add

---
+++ Application/Common/Conf/home.php
@@ -0,0 +1,6 @@
+<?php 
+	//配置信息
+	return array(
+		'address'   => '管庄'
+	)
+?>
---
+++ Application/Common/Conf/office.php
@@ -0,0 +1,11 @@
+<?php 
+	//配置信息
+	return array(
+		'address'   => '大望路',
+		'message'	=> array(
+			'sex'	=>	'男',
+			'age'	=> 	25,
+			'name'	=>	'蓝占桥'
+		)
+	)
+?>
